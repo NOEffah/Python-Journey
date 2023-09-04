bf45f92da3bde81215dbc13788ab708b8cfff75a
Nana Owusu Effah <noeffah@st.knust.edu.gh>
2023-09-04 19:12:39 +0000

    addition of new lesson on lists

---
+++ lesson 2 lists.py
@@ -0,0 +1,17 @@
+bicycle = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycle[0].title())
+message = f"My first bicycle was {bicycle[-1].title()}."
+
+print(message)
+Friends = ['eva', 'adubea', 'dagadu', 'elsie', 'lesley']
+
+friend_index = Friends[0]
+print(f"Hello {friend_index.title()}.I have missed you.")
+friend_index = Friends[1]
+print(f"Hello {friend_index.title()}.I have missed you.")
+friend_index = Friends[2]
+print(f"Hello {friend_index.title()}.I have missed you.")
+friend_index = Friends[3]
+print(f"Hello {friend_index.title()}.I have missed you.")
+
+
