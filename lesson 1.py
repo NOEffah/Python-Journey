ebe461c999e22ce72360d9062a6e59a4cc6f437c
Nana Owusu Effah <noeffah@st.knust.edu.gh>
2023-09-04 18:46:58 +0000

    prograss

---
+++ lesson 1.py
@@ -0,0 +1,25 @@
+message = "Hello Python World!"
+print(message)
+
+message = "Hello ada lovelace"
+print(message.title())
+print(message.lower())
+print(message.upper())
+
+first_name = "Ada"
+last_name = "Lovelace"
+print(f"Hello {first_name} {last_name}.Nice to meet you")
+
+favourite_lang = "Python  "
+print(favourite_lang)
+print(favourite_lang.rstrip())
+
+other_lang = "     C"
+print(other_lang)
+print(other_lang.lstrip())
+
+x, y, z = 1, 2, 3
+print(x + y + z)
+
+fav_num = 7
+print(f"My fav nim is {fav_num}")
