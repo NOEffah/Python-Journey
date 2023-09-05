56242a5ef22a48177ec601caed0c2d1405d8ec4a
Nana Owusu Effah <noeffah@st.knust.edu.gh>
2023-09-05 20:50:51 +0000

    lesson 4 - journey through if else statements

---
+++ lesson 4 if else.py
@@ -0,0 +1,14 @@
+cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+for car in cars:
+    if car == 'toyota':
+        print(car.upper())
+    else:
+        print(car.title())
+
+cer = 'bmw'  # for assignment
+print(cer == 'bmw')  # for equality
+print(cer == 'audi')
+print(cer == 'Bmw')
+
+
